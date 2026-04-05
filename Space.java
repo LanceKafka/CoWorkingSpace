85b27d96b73e9db81f658543a6ab1b9ac6df9cd6
ynanian <124759889+Misturugi@users.noreply.github.com>
2026-04-05 12:50:24 +0000

    Space.java

---
+++ Space.java
@@ -0,0 +1,26 @@
+public class Space {
+    private String spaceId;
+    private double fixedRate;
+    private boolean isOccupied;
+
+    public Space(String spaceId, double fixedRate) {
+        this.spaceId = spaceId;
+        this.fixedRate = fixedRate;
+        this.isOccupied = false;
+    }
+
+    public double calculateBill(int hrs) {
+        return hrs * fixedRate;
+    }
+
+    public void setOccupied(boolean status) {
+        this.isOccupied = status;
+    }
+
+    public String getSpaceId() { 
+        return spaceId; 
+        }
+    public boolean isOccupied() { 
+        return isOccupied; 
+        }
+}
