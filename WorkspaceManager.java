a334f0bf850083b58f47a339fcb8f3587517581e
patrick <johnpatrickbutardo@gmail.com>
2026-04-06 08:51:44 +0800

    Change age limit in registerMember method
    Updated age validation in registerMember method.

--- WorkspaceManager.java
+++ WorkspaceManager.java
@@ -70,7 +70,7 @@ public class WorkspaceManager {
     }
 
     public void registerMember(String name, int age, String user, String pass) {
-        if (age <= 17) {
+        if (age <= 18) {
             System.out.println("[!] Invalid age input.");
             return;
         }
