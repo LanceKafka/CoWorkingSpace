b5f765f19603b5c32e18778b57aebef77c508656
patrick <johnpatrickbutardo@gmail.com>
2026-05-03 20:34:59 +0800

    Refactor Member class to include age attribute

--- Member.java
+++ Member.java
@@ -1,31 +1,36 @@
+package org.example;
+
 public class Member {
     private int memberId;
     private String name;
+    private int age;
     private int pin;
     private double bal;
 
-    public Member(int memberId, String name, int pin, double bal) {
+    public Member(int memberId, 
+                  String name, 
+                  int age, 
+                  int pin, 
+                  double bal) {
         this.memberId = memberId;
         this.name = name;
+        this.age = age;
         this.pin = pin;
         this.bal = bal;
     }
 
-    public double getBalance() { 
-        return bal; 
-        }
-    public void setPin(int newPin) { 
-        this.pin = newPin; 
-        }
+    public double getBalance() { return bal; }
+    
+    public void setBalance(double bal) { this.bal = bal; }
+    
+    public void setPin(int newPin) { this.pin = newPin; }
 
-    public int getMemberId() { 
-        return memberId; 
-        }
-    public String getName() { 
-        return name; 
-        }
-    public int getPin() { 
-        return pin; 
-        }
-
+    public int getMemberId() { return memberId; }
+    
+    public String getName() { return name; }
+    
+    public int getAge() { return age; }
+    
+    public int getPin() { return pin; }
+    
 }
